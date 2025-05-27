0544d8152c45a02a8f9fa9f773ddd42fc4921e53
Tim Afanasiev <tafanasi@c4r1s3.42warsaw.pl>
2025-05-27 17:18:30 +0200

    parser structs added

---
+++ parser/parser.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tafanasi <tafanasi@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 17:14:47 by tafanasi          #+#    #+#             */
+/*   Updated: 2025/05/27 17:16:50 by tafanasi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PARSER_H
+# define PARSER_H
+
+# include "../minishell.h"
+
+typedef enum e_redirect_type {
+    REDIR_IN,     // <
+    REDIR_OUT,    // >
+    REDIR_APPEND, // >>
+    HEREDOC       // <<
+}   t_redirect_type;
+
+
+typedef struct s_redirect {
+    t_redirect_type type;
+    char            *file;
+    struct s_redirect *next;
+}   t_redirect;
+
+
+typedef struct s_cmd {
+    char          **args;         // argv-like
+    t_redirect    *in_redir;      // linked list of input redirs
+    t_redirect    *out_redir;     // linked list of output redirs
+    struct s_cmd  *next;          // for pipelines: `cmd1 | cmd2 | cmd3`
+}   t_cmd;
+
+
+typedef struct s_shell_input {
+    t_cmd   *cmds;      // linked list of all pipeline commands
+    int     is_valid;   // optional, can be useful
+}   t_shell_input;
+
+
+# endif
