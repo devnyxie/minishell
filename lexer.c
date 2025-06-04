677f3b0f6b5dfc3f18dd5aa3ff0574a770380e22
Tim Afanasiev <soupsoupchannel@gmail.com>
2025-06-04 16:21:04 +0200

    empty input segfault fixed

--- lexer.c
+++ lexer.c
@@ -5,20 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tafanasi <tafanasi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: Invalid date        by                   #+#    #+#             */
-/*   Updated: 2025/06/04 16:11:07 by tafanasi         ###   ########.fr       */
+/*   Created: 2025/06/04 16:15:33 by tafanasi          #+#    #+#             */
+/*   Updated: 2025/06/04 16:17:55 by tafanasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "minishell.h"
 
 static char	*search_cmd_path(char *path, char *cmd)
 {
-	char	**paths;
-	char	*full_path;
-	char	*tmp;
-	int		i;
 	char	**paths;
 	char	*full_path;
 	char	*tmp;
@@ -39,94 +34,10 @@ static char	*search_cmd_path(char *path, char *cmd)
 		{
 			free_2d(paths);
 			return (tmp);
-			free_2d(paths);
-			return (tmp);
 		}
-		free(tmp);
 		free(tmp);
 		i++;
 	}
-	return (NULL);
-}
-
-// Recursively launch each command in the pipeline.
-// 'prev_fd' is the read end of the previous pipe (or -1 for the first command).
-int start_process(t_cmd *cmd, int prev_fd, t_shell *shell)
-{
-    int pipefd[2] = { -1, -1 };
-    int pid;
-
-    if (!cmd->next && ft_strcmp(cmd->name, "exit") == 0)
-        exit(0);
-    // If there’s a next command, create a new pipe for this pair.
-    if (cmd->next)
-        if (pipe(pipefd) < 0)
-            perror("pipe"), exit(1);
-
-    pid = fork();
-    if (pid < 0) {
-        perror("fork");
-        exit(1);
-    }
-
-    if (pid == 0) {
-        // -------- CHILD --------
-        // 1) If there was a previous pipe, dup its read end onto stdin.
-        if (prev_fd != -1) {
-            dup2(prev_fd, STDIN_FILENO);
-            close(prev_fd);
-        }
-
-        // 2) If there’s a next command, dup this pipe’s write end onto stdout.
-        if (cmd->next) {
-            close(pipefd[0]);          // close unused read end
-            dup2(pipefd[1], STDOUT_FILENO);
-            close(pipefd[1]);
-        }
-
-        // 3) Now we’re in the right process and the fds are hooked up.
-        //    Check if it’s a builtin:
-        for (int i = 0; shell->builtins[i].name; i++) {
-            if (ft_strcmp(cmd->name, shell->builtins[i].name) == 0) {
-                shell->builtins[i].fn(cmd->args);
-                // exit(0);
-            }
-        }
-
-        // 4) Otherwise it’s an external: resolve path, then execve.
-        char *path = search_cmd_path(shell->path, cmd->name);
-        if (path) {
-            execve(path, cmd->args, shell->envp);
-            perror("execve");
-        } else {
-            fprintf(stderr, "%s: command not found\n", cmd->name);
-        }
-        exit(1);
-    }
-
-    // -------- PARENT --------
-    // 1) Close any fds we don’t need:
-    if (prev_fd != -1)
-        close(prev_fd);
-    if (cmd->next) {
-        close(pipefd[1]);  // parent only needs the read end for the next child
-    }
-
-    // 2) Recurse/loop to launch next command, passing this pipe’s read end.
-    if (cmd->next) {
-        start_process(cmd->next, pipefd[0], shell);
-    }
-
-    // 3) Wait for this child to finish.
-    waitpid(pid, NULL, 0);
-    return 0;
-}
-
-
-// gets parsed input from shell->parsed_input and starts the commands
-void	lexer(t_shell *shell, t_cmd *cmd)
-{
-	start_process(cmd, -1, shell);
 	return (NULL);
 }
 
--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: tafanasi <tafanasi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 15:59:58 by tafanasi          #+#    #+#             */
-/*   Updated: 2025/06/04 15:45:18 by tafanasi         ###   ########.fr       */
+/*   Updated: 2025/06/04 16:20:40 by tafanasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,30 +30,26 @@ void	setup_signals(void)
 
 void	await_input(t_shell *shell)
 {
-	char			*input;
-	// t_cmd			*current_cmd;
-	// int				pids[shell->parsed_input->cmds_count];
-
+	char	*input;
 	while (1)
 	{
 		input = readline("minishell$ ");
 		if (input)
 		{
 			shell->parsed_input = parser(input);
-			if (!shell->parsed_input)
-				custom_error("Parser failed\n");
-			lexer(shell, shell->parsed_input->first_cmd);
+			if (shell->parsed_input->first_cmd != NULL)
+				lexer(shell, shell->parsed_input->first_cmd);
 		}
 		free_shell_input(shell->parsed_input);
 	}
-	
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	if(argc > 1 || argv[1])
-		custom_error("Arguments are not supported");
 	t_shell	*shell;
+
+	if (argc > 1 || argv[1])
+		custom_error("Arguments are not supported");
 	shell = init_shell(envp);
 	setup_signals();
 	await_input(shell);
