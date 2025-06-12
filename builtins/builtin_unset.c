46f80a64e21a4d2f9a5abed7d934e54f8f228862
Milos Mitkovic <mitkovic_m@hotmail.com>
2025-06-12 17:07:48 +0200

    Add unset builtin file and structure

--- builtins/builtin_unset.c
+++ builtins/builtin_unset.c
@@ -11,6 +11,18 @@ int	builtin_unset(t_shell *shell, char **args)
 		printf("unset: not enough arguments\n");
 		return (1);
 	}
+
+	// cmd->args[i]
+	while (args[i])
+	{
+		if (!is_valid_identifier(args[i]))
+		{
+			pritnf("unset: `%s': not a valid identifier\n", args[i]);
+			return (1);
+		}
+		unset_env_var(args[i], shell->envp);
+		i++;
+	}
 	return (0);
 }
 
