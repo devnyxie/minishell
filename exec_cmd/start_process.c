d2b6bfe56d0a45a44e0fa3a3583fa744395c0d05
Tim Afanasiev <soupsoupchannel@gmail.com>
2025-08-06 15:37:18 +0200

    more memory fixes

--- exec_cmd/start_process.c
+++ exec_cmd/start_process.c
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   start_process.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmitkovi <mmitkovi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tafanasi <tafanasi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 17:18:09 by tafanasi          #+#    #+#             */
-/*   Updated: 2025/08/05 14:52:11 by mmitkovi         ###   ########.fr       */
+/*   Updated: 2025/08/06 14:32:58 by tafanasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec_cmd.h"
 
-static void	handle_exit_if_needed(t_cmd *cmd)
-{
-	if (!cmd->next && ft_strcmp(cmd->name, "exit") == 0)
-		exit(0);
-}
+// static void	handle_exit_if_needed(t_cmd *cmd)
+// {
+// 	if (!cmd->next && ft_strcmp(cmd->name, "exit") == 0)
+// 		exit(0);
+// }
 
 static void	create_pipe_if_needed(t_cmd *cmd, int pipefd[2])
 {
@@ -45,7 +45,8 @@ int	start_process(t_cmd *cmd, int prev_fd, t_shell *shell, char **args)
 	int	status;
 	int	exit_code;
 
-	handle_exit_if_needed(cmd);
+	ft_putstr_fd("exit goes here\n", 1);
+	// handle_exit_if_needed(cmd);
 	if (is_parent_builtin(shell, cmd))
 		execute_parent_builtin(shell, args, cmd);
 	else
